42d59a6e0ecb3d4c3b8df7af506b183f085bd60b
Oleg <swigerd2l@gmail.com>
2024-05-17 12:43:17 +0300

    leave bot if no members in vc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { getVoiceConnection } from '@discordjs/voice';
 import { Client, IntentsBitField } from 'discord.js';
+import { PlayerManager } from './PlayerManager';
 import { commands } from './commands';
+import { queue } from './commands/play';
 import { config } from './config';
 import { deployCommands } from './deploy-commands';
 
@@ -18,6 +21,22 @@ client.once('ready', () => {
 
 client.on('guildCreate', async (guild) => {
   await deployCommands({ guildId: guild.id });
+});
+
+client.on('voiceStateUpdate', (_, newState) => {
+  if (!newState.channel?.members.size) {
+    const connection = getVoiceConnection(newState.guild.id);
+
+    const player = PlayerManager.getPlayerInstance;
+
+    player?.stop();
+
+    if (queue.has(newState.guild.id) && queue.get(newState.guild.id).length) {
+      queue.set(newState.guild.id, []);
+    }
+
+    connection?.destroy();
+  }
 });
 
 client.on('interactionCreate', async (interaction) => {
